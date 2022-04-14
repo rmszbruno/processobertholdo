01545134194837b1cc05f4236fa8caa7d66d6feb
brunormsz <rmszbruno@gmail.com>
2022-04-14 00:15:16 -0300

    adicionando v1.0

--- script.js
+++ script.js
@@ -15,6 +15,44 @@ $(document).ready(function(){
           }
       }
   })
+
+  $('.bannerprincipal').owlCarousel({
+    items:1,
+    margin:10,
+    autoWidth:true,
+    autoHeight:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
+
+  var $contactDireita = $('#contactdireita');
+  var $contactEsquerda = $('#contactesquerda');
+  var $optionsFooter = $('.optionsfooter');
+
+$(window).resize(function() {
+  if (window.innerWidth <= 900) $contactDireita.addClass('text-center');
+
+});
+
+$(window).resize(function() {
+    if (window.innerWidth <= 900) $contactEsquerda.addClass('text-center');
+  
+  });
+
+$(window).resize(function() {
+    if (window.innerWidth <= 900) $optionsFooter.addClass('text-center');
+  
+});
+
   });
 
   
